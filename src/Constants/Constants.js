1bafa6f00da06d18e475e4dc4a74ddf725a553f6
Ahmed-Abbasi-Official <farhad.khi17@gmail.com>
2024-12-10 23:49:47 +0500

    init

--- src/Constants/Constants.js
+++ src/Constants/Constants.js
@@ -190,15 +190,15 @@ const certifications = [
     },
     {
         img: certificate3,
-        certificateProvider: "ASME NUST PNEC",
-        certificationName: "MATLAB Training Work Shop",
-        issuedDate: "October 2023"
+        // certificateProvider: "ASME NUST PNEC",
+        // certificationName: "MATLAB Training Work Shop",
+        // issuedDate: "October 2023"
     },
     {
         img: certificate4,
-        certificateProvider: "ASME NUST PNEC",
-        certificationName: "MATLAB Training Work Shop",
-        issuedDate: "October 2023"
+        // certificateProvider: "ASME NUST PNEC",
+        // certificationName: "MATLAB Training Work Shop",
+        // issuedDate: "October 2023"
     },
     {
         img: certificate5,
